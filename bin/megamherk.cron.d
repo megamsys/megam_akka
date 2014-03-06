8f8b8343b77de053759f04b414c0486023af5470
Kishorekumar Neelamegam <nkishore@megam.co.in>
2014-03-06 13:49:55 +0530

    cron gets placed in /etc/cron.d with 644 permissions. Testing time..

--- bin/megamherk.cron.d
+++ bin/megamherk.cron.d
@@ -1,9 +1,9 @@
 # This is a cron that wakes up and runs every 10 minutes. 
-# The cron activates as cleanup script herk_stash 
+# The cron activates a cleanup script herk_stash 
 #
 # Megam.  http://www.gomegam.com
 #
 # Copyright 2014, Megam systems
 #
 MAILTO=""
-* * * * * root @@MEGAM_HOME@@/bin/herk_stash
+*/10 * * * * root @@MEGAM_HOME@@/bin/herk_stash
